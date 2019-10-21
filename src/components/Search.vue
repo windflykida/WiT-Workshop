585892917b39159015056cb3c00485405263cf24
Anna Jachym <ania.jachym@gmail.com>
2019-10-21 09:33:43 +0200

    Seperation of App.vue components

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="form-group">
+ 
+  <input 
+      class="form-control"
+      :class="{
+        'is-invalid': isInvalid
+        }"
+      type="text"
+      :value="value"
+      @input="sendData($event)"
+   />
+
+  </div>  
+</template>
+
+<script>
+export default {
+    props:{
+    value:{
+        type: String,
+        requried: true,
+    }
+    },
+    computed: {
+    isInvalid () {
+      return  /[^a-z]/i.test(this.value)
+        
+    },
+    },
+    methods:{
+        sendData ($event) {
+            this.$emit('input', $event.target.value)
+        }
+    }
+}
+</script>
